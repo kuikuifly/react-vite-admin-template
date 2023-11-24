4ae757a6ac072ec604fff8631b7f3e7c79f79534
kuikuifly <836667577@qq.com>
2023-11-24 16:58:12 +0800

    fix: clear TODO

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { /* BrowserRouter */ HashRouter } from 'react-router-dom';
-/**
-  BrowserRouter模式需要服务器配置来处理不同路径的请求。服务器需要返回正确的内容，
-  以使应用程序的路由能够正常工作。一般来说，服务器会配置一个捕获所有路径的路由，并将
-  它们重定向到index.html，以便应用程序可以正确解析和处理路径。这样，无论用户访问哪
-  个路径，都会返回相同的index.html文件，并由客户端的JavaScript来处理路由逻辑。
-
-  后台管理系统不涉及seo，使用BrowserRouter
-*/
-
 import 'virtual:svg-icons-register';
 // 全局css
 import '@/assets/css/index.less';
